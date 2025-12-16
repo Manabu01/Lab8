22f3458af6a8656ddb173200eba6713185603272
Poomipat <poomipatboonkong@gmail.com>
2025-12-16 16:17:06 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,43 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int age, height, bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age > 25 && age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 5e8){
+            character = "Jinbe";
+        }else{
+            character = "Franky";
+        }
+    }else if(age > 60){
+        character = "Brook";
+    }else{
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 180 && height >= 100){
+            character = "Usopp";
+        }else if(height < 100){
+            character = "Chopper";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 11e8){
+                character = "Zoro";
+            }else{
+                character = "Sanji";
+            }
+        }
+    }
+    cout << "Your character = " << character;
+    return 0;
+}
